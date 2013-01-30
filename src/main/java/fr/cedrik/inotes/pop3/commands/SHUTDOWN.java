1230b4977e9b5a36154f7dc5879ffb8c0548ec2e
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2013-01-30 11:38:07 +0100

    Log POP3 shutdown command for auditing

--- src/main/java/fr/cedrik/inotes/pop3/commands/SHUTDOWN.java
+++ src/main/java/fr/cedrik/inotes/pop3/commands/SHUTDOWN.java
@@ -29,7 +29,10 @@ public class SHUTDOWN extends BasePOP3Command implements POP3Command {
 	public Iterator<String> call(Context context) throws IOException {
 		String magicPassword = context.inputArgs;
 		if (context.pop3Properties.getPOP3ShutdownSecret().equals(magicPassword)) {
+			logger.warn("Shutting down POP3 server on command from " + context.userName);
 			POP3Server.shutdown();
+		} else {
+			logger.warn("Invalid POP3 server shutdown command secret from " + context.userName);
 		}
 		// no else: we don't want to give any indication of "password" failure...
 		return new IteratorChain<String>(ResponseStatus.POSITIVE.toString("good bye!"));
